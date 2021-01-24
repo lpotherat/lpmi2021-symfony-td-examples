9a357931d4bbcf1f7d8e908b74387e1e3519ee62
Léonard POTHERAT <l.potherat@hotmail.com>
2021-01-24 17:59:37 +0100

    Added database example + IDE parameters

--- src/Repository/TrackRepository.php
+++ src/Repository/TrackRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository;
 
+use App\Entity\Record;
 use App\Entity\Track;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -19,22 +20,30 @@ class TrackRepository extends ServiceEntityRepository
         parent::__construct($registry, Track::class);
     }
 
-    // /**
-    //  * @return Track[] Returns an array of Track objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    /**
+     * Retourne la liste des pistes d'un disque à partir de l'identifiant du disque
+     * @param $recordId
+     * @return int|mixed|string
+     */
+    public function findByRecordId($recordId)
     {
-        return $this->createQueryBuilder('t')
-            ->andWhere('t.exampleField = :val')
-            ->setParameter('val', $value)
+        return
+            //Récupération d'un querybuilder préparamétré avec
+            //un select sur la table de l'entité en cours
+            $this->createQueryBuilder('t')
+            ->join(Record::class,'r')
+            //Ajout d'une condition dans la clause WHERE
+            ->where('r.id = :recordId')
+            ->setParameter('recordId', $recordId)
+            //Facultatif : Ajout d'un tri sur l'identifiant de piste ascendant
             ->orderBy('t.id', 'ASC')
-            ->setMaxResults(10)
+            //Création de la requête
             ->getQuery()
+            //Récupération du résultat formatté, en tableau de Track
             ->getResult()
         ;
     }
-    */
+
 
     /*
     public function findOneBySomeField($value): ?Track
